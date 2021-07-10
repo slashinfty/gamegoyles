9322ddb8c4c42d5f6bb1cbbf0c539143b7cdbf36
Matt Braddock <matt.braddock@gmail.com>
2021-07-10 17:26:21 -0400

    now playing

---
+++ public/music.js
@@ -0,0 +1,9 @@
+const updateNowPlaying = async () => {
+    const response = await fetch('https://rainwave.cc/api4/info_all?sid=2');
+    const object = await response.json();
+    const songInfo = object.all_stations_info['2'];
+    document.getElementById('now-playing').innerHTML = '<u>Now Playing</u>Song: ' + songInfo.title + '<br>Artist(s): ' + songInfo.artists + '<br>Album: ' + songInfo.album + '<br>All music from OC Remix';
+}
+
+updateNowPlaying();
+setInterval(updateNowPlaying, 8000);
