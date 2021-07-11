65d39d0f1dde103ec57d83fc40a1bac80fcb141e
Matt Braddock <matt.braddock@gmail.com>
2021-07-10 21:27:34 -0400

    simple fixes

--- public/music.js
+++ public/music.js
@@ -2,7 +2,7 @@ const updateNowPlaying = async () => {
     const response = await fetch('https://rainwave.cc/api4/info_all?sid=2');
     const object = await response.json();
     const songInfo = object.all_stations_info['2'];
-    document.getElementById('now-playing').innerHTML = '<u>Now Playing</u>Song: ' + songInfo.title + '<br>Artist(s): ' + songInfo.artists + '<br>Album: ' + songInfo.album + '<br>All music from OC Remix';
+    document.getElementById('now-playing').innerHTML = '<u>Now Playing</u>Song: ' + songInfo.title + '<br>Artist(s): ' + songInfo.artists + '<br>Album: ' + songInfo.album;
 }
 
 updateNowPlaying();
