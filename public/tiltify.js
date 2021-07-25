8f7ec8b80b2b2f4c90f155fdf20576fe637466f5
Matt Braddock <matt.braddock@gmail.com>
2021-07-24 21:02:41 -0400

    better money display

--- public/tiltify.js
+++ public/tiltify.js
@@ -1,6 +1,13 @@
 const tiltifyClient = new TiltifyClient(process.env.TILTIFY_TOKEN);
 
-const getDonationTotal = async () => await tiltifyClient.Campaigns.get(process.env.TILTIFY_CAMPAIGN, data => document.getElementById('donation-total').innerText = '$' + data.amountRaised);
+const printMoney = amount => {
+    let text = '$' + amount;
+	if (/\.\d$/.test(text)) text += '0';
+	else if (!/\./.test(text)) text += '.00';
+	return text;
+}
+
+const getDonationTotal = async () => await tiltifyClient.Campaigns.get(process.env.TILTIFY_CAMPAIGN, data => document.getElementById('donation-total').innerText = printMoney(data.amountRaised));
 
 getDonationTotal();
 setInterval(getDonationTotal, 30000);
